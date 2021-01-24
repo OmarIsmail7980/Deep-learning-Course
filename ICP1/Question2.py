bb3b812170e02551c389bf8b496ce3a90d91e520
Omar Ismail <71655271+OmarIsmail7980@users.noreply.github.com>
2021-01-23 22:06:46 -0600

    Add files via upload

---
+++ ICP1/Question2.py
@@ -0,0 +1,26 @@
+
+# PART 1
+p = input("welcome, please enter an string:")
+
+# remove 2 characters of the string
+result = p[:-2]
+
+#print the reversed string
+print(result[::-1])
+
+
+
+
+112# PART 2
+num1 = input ("Please enter a number:")
+num2 = input("Please enter a number:")
+
+
+firstNum = int(num1)
+secondNum = int(num2)
+
+
+print("The resulted addtion: {}" .format(firstNum + secondNum))
+print("The resulted subtraction: {}" .format(firstNum - secondNum))
+print("The resulted multiplication: {}" .format(firstNum * secondNum))
+print("The resulted Division: {}" .format(firstNum / secondNum))
---
+++ ICP1/Question3.py
@@ -0,0 +1,9 @@
+
+# take input senetence from user
+input = input("Please write a sentence:")
+
+#replace the words
+result = input.replace("python", "pythons")
+
+print(result)
+
